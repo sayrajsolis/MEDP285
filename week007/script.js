5bd0dd48e3d7ed5315c7dee840684c0678880fda
sayrajsolis <sayrajsolis@users.noreply.github.com>
2020-10-20 17:27:56 -0400

    midterm

--- week007/script.js
+++ week007/script.js
@@ -42,6 +42,7 @@
   //Abilities
   const select = document.querySelector('select');
   const abilityDESC = document.getElementById("ability-description");
+  const abilityDEMO = document.getElementById("ability-demo");
 
 
 select.addEventListener('change', setAbility);
@@ -50,13 +51,26 @@ select.addEventListener('change', setAbility);
 
     if (choice === '') {
       abilityDESC.innerHTML = '';
+      abilityDEMO.innerHTML = '';
     }
 
     else if (choice === 'alarmbot') {
       abilityDESC.innerHTML = 
-        "Q - ALARMBOT <br> EQUIP a covert Alarmbot. FIRE to deploy a bot that hunts down enemies that get in range. After reaching its target, the bot explodes, applying Vulernable. HOLD EQUIP to recall a deployed bot.";
+        "<br><b>Q - ALARMBOT </b><br><br> EQUIP a covert Alarmbot. FIRE to deploy a bot that hunts down enemies that get in range. After reaching its target, the bot explodes, applying Vulernable. HOLD EQUIP to recall a deployed bot.";
+      abilityDEMO.innerHTML = "<iframe src = 'https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt9a8fa11ac887550e/5f2203522f812a7c016f5651/AlarmBot_LowQuality.mp4'></iframe>";
     }
     else if (choice === 'turret') {
-      abilityDESC.innerHTML = "E - TURRET EQUIP a Turret. FIRE to deploy a turret that fires at enemies in a 180 degree cone. HOLD EQUIP to recall the deployed turret."
+      abilityDESC.innerHTML = "<br><b>E - TURRET</b> <br><br> EQUIP a Turret. FIRE to deploy a turret that fires at enemies in a 180 degree cone. HOLD EQUIP to recall the deployed turret."
+      abilityDEMO.innerHTML = "<iframe src= 'https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt0fe3462ae9a025a4/5f220396074360086ccdd908/Turret_LowQuality.mp4'></iframe>"
+    }
+    else if(choice === 'nanoswarm'){
+      abilityDESC.innerHTML = 
+        "<br><b>C - NANOSWARM</b><br><br>EQUIP a Nanoswarm grenade. FIRE to throw the grenade. Upon landing, the Nanoswarm goes covert. ACTIVATE the Nanoswarm to deploy a damaging swarm of nanobots."
+      abilityDEMO.innerHTML = "<iframe src = 'https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt4aed833e1b0df155/5f2204694be7297d7e6c8449/Grenade_LowQuality.mp4'></iframe>"
+    }
+    else if(choice === 'lockdown'){
+      abilityDESC.innerHTML = 
+        "<br><b>X - LOCKDOWN</b><br><br>EQUIP the Lockdown device. FIRE to deploy the device. After a long windup, the device Detains all enemies caught in the radius. The device can be destroyed by enemies."
+      abilityDEMO.innerHTML = "<iframe src = 'https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltf74d72b162a14692/5f2204ab8ff50d070ad2d192/Ultimate_LowQualityV02.mp4'></iframe>"
     }
   }
